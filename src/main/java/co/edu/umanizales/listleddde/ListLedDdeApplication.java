68bfd1fde62e183901bfbb785e5efe003f11ff74
LuisMalte <127308566+LuisMalte@users.noreply.github.com>
2023-05-08 17:49:14 -0500

    Prueba1

---
+++ src/main/java/co/edu/umanizales/listleddde/ListLedDdeApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.listleddde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListLedDdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListLedDdeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/umanizales/listleddde/ListLedDdeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.listleddde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListLedDdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
